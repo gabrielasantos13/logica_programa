470107dedd9024ded5abb238959039cc83a835c7
GABRIELA SILVA SANTOS <gabriela.ssantos117@senacsp.edu.br>
2025-01-13 20:17:35 -0300

    aula 13/01

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Ex03 - Escrever programa que solicite dois numeros inteiros para o usuário e exiba a soma dos número
---
+++ ex03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ex03 - Escrever programa que solicite dois numeros inteiros para o usuário e exiba a soma dos número
+
+Console.WriteLine("Digite o primeiro número: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número: ");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o quarto número: ");
+int numero4 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("A soma é: " + (numero1 + numero2 + numero3 + numero4));
+
+Console.WriteLine($"A soma é:  {numero1 + numero2 + numero3 + numero4} ");
